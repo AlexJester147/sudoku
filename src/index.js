02505e922fce3e3a365c4b6762fb7b1e10ff3b3f
AlexJester147 <vardy2016@lenta.ru>
2021-07-17 12:04:31 +0300

    feat : add solution to the task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,37 @@
-module.exports = function solveSudoku(matrix) {
-  // your solution
+module.exports = function solveSudoku(array){
+
+  function valid(array, row, col, value){
+
+for (let i=0; i < 9; i++){
+  const m = 3 * Math.floor(row/3) + Math.floor(i/3);
+  const n = 3 * Math.floor(col/3) + i%3;
+  if (array[row][i] == value || array[i][col] == value || array[m][n] == value){
+    return false;
+  }
 }
+return true;
+}
+
+
+for (let i=0; i < 9; i++){
+  for (let j=0; j < 9; j++){
+    if (array[i][j] == 0){
+      for (let k=1; k <=9; k++){
+        if (valid(array, i, j, k)){
+          array[i][j]=k;
+          if(solveSudoku(array)){
+            return array;
+          } 
+          else {
+            array[i][j]=0;
+          }
+        }
+      }
+      return false;
+    }
+  }
+}
+
+return array;
+
+};
